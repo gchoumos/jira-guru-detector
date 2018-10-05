186666fbb7d0b7a9c755834d7bd68486cb88de2a
choumosg <g.choumos@gmail.com>
2018-10-05 17:05:16 +0300

    Apply changes to specific columns and not the whole dataset. Make email regexp more efficient. Avoid inplace updates.

--- dataPreprocessor/preprocessor.py
+++ dataPreprocessor/preprocessor.py
@@ -27,19 +27,25 @@ class DataPreprocessor(object):
     def clean_comment_newlines(self):
         # Replace carriage returns and newlines with space
         print("Removing newlines and carriage returns ...")
-        self.comments.replace([r'\n',r'\r'],' ', regex=True, inplace=True)
+        self.comments[['comment']] = self.comments[['comment']].replace([r'\n',r'\r'],' ', regex=True)
 
     def remove_multiple_spaces(self):
         print("Removing multiple spaces ...")
-        self.comments.replace(r'\s+',' ', regex=True, inplace=True)
+        self.comments[['comment']] = self.comments[['comment']].replace(r'\s+',' ', regex=True)
 
     def remove_urls(self):
         print("Removing URLs ...")
-        self.comments.replace(r'((https?:\/\/)|(www\.))[^ \n\r]*', '', regex=True, inplace=True)
+        self.comments[['comment']] = self.comments[['comment']].replace(r'((https?:\/\/)|(www\.))[^ \n\r]*', '', regex=True)
 
     def remove_emails(self):
         print("Removing e-mails ...")
-        self.comments.replace(r'(\w|\.|-)+@(\w|\.|\-)+\.\w+', '', regex=True, inplace=True)
+        self.comments[['comment']] = self.comments[['comment']].replace(r'[\w\.-]+@[\w\.-]+\.\w+', '', regex=True)
+
+    # def extract_quotes(self):
+    #     print("Extracting text in {quote} tags ...")
+    #     # Create first the new column with the quote contents from comments
+
+
 
     def comments_to_csv(self):
         self.comments.to_csv('{0}/comments.csv'.format(self.output_path))
@@ -63,6 +69,6 @@ comments.csv (WIL-20000 to WIL-57199)
 Original    - Size: 782056 KB - Lines: 12864079
 No newlines - Size: 709968 KB - Lines:   456201
 No URLs     - Size: 698696 KB
-No emails   - Size: 692364 KB
+No emails   - Size: 695204 KB
 
 """
